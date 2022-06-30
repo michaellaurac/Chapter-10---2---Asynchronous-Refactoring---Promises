7651712a12a4c6c4f543dbbb4b403280b6b6419d
Michaël L <michaellaurac@gmail.com>
2022-06-30 13:16:55 +0200

    Addining testing of both promises with tape and testdouble

--- promises_test.js
+++ promises_test.js
@@ -14,3 +14,32 @@ test("verifies the test file name", (assert) => {
 });
 
 // functional tests
+test("verifies that addOne yields 4 when given 3 as a parameter", (assert) => {
+  addOne(3).then((result) => {
+    assert.strictEqual(result, 4);
+    assert.end();
+  });
+});
+
+test("verifies that four takes 4 as a parameter and waits 500ms", (assert) => {
+  four(3).then((result) => {
+    assert.strictEqual(result, 4);
+    assert.end();
+  });
+});
+
+test("verifies that four yields 5 after 500ms when given 4 as a parameter", (assert) => {
+  testDouble.replace(console, "log");
+  four()
+    .then(addOne)
+    .then(console.log)
+    .then(() => {
+      testDouble.verify(console.log(5));
+      assert.pass();
+      testDouble.reset();
+      assert.end();
+    }).catch((e) => {
+      testDouble.reset();
+      console.log(e);
+    });
+});
